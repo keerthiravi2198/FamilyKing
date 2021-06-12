3a306efebf19de604409a8d247d9e6a54ecc0e43
Keerthana Ravichandran <keerthiravi2198@gmail.com>
2021-06-12 13:10:58 +0530

    Create Geektrust.java

---
+++ Geektrust.java
@@ -0,0 +1,14 @@
+
+
+public class Geektrust {
+	
+
+	public static void main(String[] args) {
+		for(int i=0;i<8;i++) {
+			System.out.println("Hey geek trust");
+			System.out.println("heyyyyyy");
+		}
+		
+
+	}
+}
